44fc76316e4beb536e50e18bed048986188eddeb
Aarys Sahler <aarys.sahler@gmail.com>
2024-02-04 17:49:02 +0100

    layout

---
+++ layouts/precise.vue
@@ -0,0 +1,22 @@
+<script>
+  import "~/assets/css/precise.css"
+</script>
+
+<template>
+  <div>
+    <div id="headerDiv">
+      <div id="header">
+	<div id="root" @click="navigateTo('/')"> 
+	  <slot name="root"> asahler </slot>
+	</div> 
+	/<slot name="page">
+	   500_page
+	</slot>
+      </div>
+    </div>
+
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,17 @@
+<script>
+  definePageMeta({
+    layout: false
+  })
+</script>
+
+
 <template>
-  hello :D
+  <div>
+    <NuxtLayout name="precise">
+      <template #root>asahler</template>
+      <template #page>about_me.md </template>
+
+      Rest of the page :D
+    </NuxtLayout>
+</div>
 </template>
